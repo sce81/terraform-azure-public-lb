e051b308edf491eddac978390c633c646af137e2
Simon Elliott <simon.elliott@hashicorp.com>
2024-04-03 12:37:49 +0100

    develop - changed identity profile

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "main" {
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   allocation_method   = "Static"
-  domain_name_label   = data.azurerm_resource_group.main.name
+  domain_name_label   = "sce-${data.azurerm_resource_group.main.name}"
 
   tags = merge(
     local.common_tags, var.extra_tags,
@@ -18,8 +18,8 @@ resource "azurerm_lb" "main" {
   sku_tier            = var.lb_sku_tier
 
   frontend_ip_configuration {
-    name                 = "${var.name}-public-ip"
-    zones                = var.lb_zones
+    name  = "${var.name}-public-ip"
+    zones = var.lb_zones
     public_ip_address_id = azurerm_public_ip.main.id
   }
   tags = merge(
@@ -34,8 +34,8 @@ resource "azurerm_lb_rule" "example" {
   name                           = "${var.name}-LBRule"
   protocol                       = "Tcp"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
-  frontend_port                  = 80
-  backend_port                   = 80
+  frontend_port                  = 22
+  backend_port                   = 22
   frontend_ip_configuration_name = "${var.name}-public-ip"
 
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "lb_pool" {
   value = [azurerm_lb_backend_address_pool.main.id]
 }
+output "id" {
+  value = azurerm_lb.main.id
+}
+
+output "lb_frontend_ip" {
+  value = azurerm_lb.main.frontend_ip_configuration
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.main.ip_address
+}
+
+output "public_fqdn" {
+  value = azurerm_public_ip.main.reverse_fqdn
+}
+
